135db20b96de689d6d64ea178025fe072999526f
Mark Caldwell <caldweme@yahoo.com>
2021-09-04 15:28:18 +0200

    locations Backup

--- routes/locations.js
+++ routes/locations.js
@@ -4,7 +4,7 @@ const User = require("../models/User.model");
 const Locations = require("../models/locations.model");
 
 router.post("/save-new-loc", (req, res) => {
-  Locations.create({ longitude: req.body.newLongitude, latitude: req.body.newLatitude }).then(() => {
+  Locations.create({ name: req.body.newname, longitude: req.body.newLongitude, latitude: req.body.newLatitude }).then(() => {
     res.redirect("/userprofile");
   });
 });
