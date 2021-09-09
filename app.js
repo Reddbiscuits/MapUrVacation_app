c484d7d27e3c341f7ff5ccfef45cbc07ab010a11
Mark Caldwell <caldweme@yahoo.com>
2021-09-09 21:19:18 +0200

    use css instead of scss

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // require('./db');
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 
 const app = express();
 
 // require session
-const session = require('express-session');
+const session = require("express-session");
 
 // ADDED: require mongostore
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 
 app.use(
   session({
-    secret: 'doesnt matter',
+    secret: "doesnt matter",
     resave: false,
     saveUninitialized: true,
     cookie: { maxAge: 600000000 }, // 10 minutes
@@ -31,62 +31,48 @@ app.use(
       //mongooseConnection: mongoose.connection,
       // ttl => time to live
       // ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
-    })
+    }),
   })
 );
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require('./config')(app);
+require("./config")(app);
 
 mongoose
-  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-
-
+const app_name = require("./package.json").name;
+const debug = require("debug")(`${app_name}:${path.basename(__filename).split(".")[0]}`);
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Express - Generated with IronGenerator";
 
-
-
-const index = require('./routes/index');
-app.use('/', index);
-const locations = require('./routes/locations');
-app.use('/', locations);
-const movies = require('./routes/movies');
-app.use('/', movies);
-const user = require('./routes/userRoutes');
-app.use('/', user);
-
+const index = require("./routes/index");
+app.use("/", index);
+const locations = require("./routes/locations");
+app.use("/", locations);
+const movies = require("./routes/movies");
+app.use("/", movies);
+const user = require("./routes/userRoutes");
+app.use("/", user);
 
 module.exports = app;
